b4dde6dc1d799e28f5df36874dc8e4f86c3fe772
Sanghyun Lee <hammerha@gmail.com>
2019-12-29 22:05:36 +0100

    Add more unicode tests

--- test/unicode.test.js
+++ test/unicode.test.js
@@ -1,5 +1,51 @@
 import test from 'ava';
 
+test('codePointAt() vs. codeCharAt()', (t) => {
+  const facepalm = '🤦🏼‍♂️';
+
+  const splitedCodePoints = facepalm.split('').map((v) => v.codePointAt(0).toString(16));
+
+  t.deepEqual(splitedCodePoints, [
+    'd83e',
+    'dd26',
+    'd83c',
+    'dffc',
+    '200d',
+    '2642',
+    'fe0f',
+  ]);
+
+  const codePoints = []
+  for (let i = 0; i < facepalm.length; i++) {
+    codePoints.push(facepalm.codePointAt(i).toString(16));
+  }
+
+  // WTF?
+  t.deepEqual(codePoints, [
+    '1f926', // merged
+    'dd26', // remained
+    '1f3fc', // merged
+    'dffc', // remained
+    '200d',
+    '2642',
+    'fe0f',
+  ]);
+
+  // The UTF-16 code unit matches the Unicode code point for code points
+  // which can be represented in a single UTF-16 code unit.
+  // If the Unicode code point cannot be represented in
+  // a single UTF-16 code unit (because its value is greater than 0xFFFF)
+  // then the code unit returned will be the first part of a surrogate pair
+  // for the code point.
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+  const codeChars = []
+  for (let i = 0; i < facepalm.length; i++) {
+    codeChars.push(facepalm.charCodeAt(i).toString(16));
+  }
+
+  t.deepEqual(codeChars, splitedCodePoints);
+});
+
 test('Emoji length 🤦🏼‍♂️', (t) => {
   const facepalm = '🤦🏼‍♂️';
 
@@ -16,7 +62,8 @@ test('Emoji length 🤦🏼‍♂️', (t) => {
   ]);
 
   // Merging surrogate pairs - the result is the same as Python's.
-  t.deepEqual(Array.from(facepalm.split("").join("")).map((v) => v.codePointAt(0).toString(16)), [
+  // For some reason, Array.from() merges surrogate pairs.
+  t.deepEqual(Array.from(facepalm).map((v) => v.codePointAt(0).toString(16)), [
     '1f926',
     '1f3fc',
     '200d',
